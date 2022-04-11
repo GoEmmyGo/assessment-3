d62cd25865850ebc86f843ea7e9a0512ef640f14
Emmaline <emmalinerose19@gmail.com>
2022-04-10 23:59:09 -0500

    successful video embed, still ugly though

--- profile.js
+++ profile.js
@@ -4,13 +4,13 @@ function colorAnswer(event) {
     alert(`Right now, Lilac. But honestly, its usually a shade of yellow or blue-green and that's about as non-descript as I can get about that`)
 }
 
-function placeAnswer(event) {
+function ritualAnswer(event) {
 	event.preventDefault()
 
-    alert(`Red Rock Canyon National Conservancy, enough sweet bouldering to last a lifetime and a personal project called "Sad Robot" that I will guaranteed send next time I go, please god, I have to send it`)
+    alert(`Bouldering at Red Rock Canyon National Conservancy, enough sweet climbs to last a lifetime and a personal project called "Sad Robot" that I will guaranteed send next time I go, please god, I have to send it`)
 }
 
-function ritualAnswer(event) {
+function placeAnswer(event) {
 	event.preventDefault()
     
     alert(`Shoot, should've saved that last answer for this one. I guess if that's my favorite ritual, then being outside in the morning sun surrounded by tall trees and a slight breeze with my beautiful dogs running up to me and my partner by my side while I sip a cortado and look up is my favorite place to be`)
@@ -22,5 +22,5 @@ let placeClick = document.querySelector('#place')
 let ritualClick = document.querySelector('#ritual')
 
 colorClick.addEventListener(`click`, colorAnswer)
-placeClick.addEventListener(`click`, ritualAnswer)
-ritualClick.addEventListener(`click`, placeAnswer)    
+placeClick.addEventListener(`click`, placeAnswer)
+ritualClick.addEventListener(`click`, ritualAnswer)    
